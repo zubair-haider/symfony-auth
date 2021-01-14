31e91b368001d98bb40d9109f01601e12b000c4d
zubair-haider <zubair.haider@gigalabs.co>
2021-01-14 12:50:21 +0500

    Using api url from .env

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -8,27 +8,23 @@ use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class AuthController extends ApiController
 {
-
     public function register(Request $request, HttpClientInterface $client)
     {
         $request = $this->transformJsonBody($request);
         $password = $request->get('password');
         $email = $request->get('email');
-        $roles = $request->get('roles');
-
         if (empty($password) || empty($email)) {
-            return $this->respondValidationError("Invalid Username or Password or Email");
+            return $this->respondValidationError("Password or Email");
         }
-
+        $url = $_ENV['DATA_URL'] . "register";
         $response = $client->request(
             'POST',
-            "http://127.0.0.1:8000/register",
+            $url,
             [
                 'json' => ['email' => $email, "password" => $password, "roles" => $roles],
             ]
         );
         $data = $response->toArray();
-
         return $this->respondWithSuccess($data);
     }
 
@@ -37,10 +33,10 @@ class AuthController extends ApiController
         $request = $this->transformJsonBody($request);
         $password = $request->get('password');
         $email = $request->get('email');
-
+        $url = $_ENV['DATA_URL'] . "login";
         $response = $client->request(
             'POST',
-            "http://127.0.0.1:8000/login",
+            $url,
             [
                 'json' => ['username' => $email, "password" => $password],
             ]
