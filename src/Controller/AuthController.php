b15bd40857c21d10bab8d8d0703f931acbc18dc4
zubair-haider <zubair.haider@gigalabs.co>
2021-01-14 14:12:45 +0500

    Code refactoring

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -10,43 +10,52 @@ class AuthController extends ApiController
 {
     public function register(Request $request, HttpClientInterface $client)
     {
-        $request = $this->transformJsonBody($request);
-        $password = $request->get('password');
-        $email = $request->get('email');
-        if (empty($password) || empty($email)) {
-            return $this->respondValidationError("Password or Email");
+        try {
+            $request = $this->transformJsonBody($request);
+            $password = $request->get('password');
+            $email = $request->get('email');
+            if (empty($password) || empty($email)) {
+                return $this->respondValidationError("Password or Email");
+            }
+            $url = $_ENV['DATA_URL'] . "register";
+            $response = $client->request(
+                'POST',
+                $url,
+                [
+                    'json' => ['email' => $email, "password" => $password],
+                ]
+            );
+            $data = $response->toArray();
+            return $this->respondWithSuccess($data);
+        } catch (\Exception $e) {
+            return $this->respondValidationError("Unable to Create user");
         }
-        $url = $_ENV['DATA_URL'] . "register";
-        $response = $client->request(
-            'POST',
-            $url,
-            [
-                'json' => ['email' => $email, "password" => $password, "roles" => $roles],
-            ]
-        );
-        $data = $response->toArray();
-        return $this->respondWithSuccess($data);
     }
 
     public function login(Request $request, HttpClientInterface $client, JWTTokenManagerInterface $JWTManager)
     {
-        $request = $this->transformJsonBody($request);
-        $password = $request->get('password');
-        $email = $request->get('email');
-        $url = $_ENV['DATA_URL'] . "login";
-        $response = $client->request(
-            'POST',
-            $url,
-            [
-                'json' => ['username' => $email, "password" => $password],
-            ]
-        );
-        $data = $response->toArray();
-        $roles = $data['roles'];
-        $email = $data['username'];
-        $user = new JWTUser($email, $roles);
-        $token = $JWTManager->create($user);
-        $returnData = ["data" => $data, "token" => $token];
-        return $this->respondWithSuccess($returnData);
+        try {
+            $request = $this->transformJsonBody($request);
+            $password = $request->get('password');
+            $email = $request->get('email');
+            $url = $_ENV['DATA_URL'] . "login";
+            $response = $client->request(
+                'POST',
+                $url,
+                [
+                    'json' => ['username' => $email, "password" => $password],
+                ]
+            );
+            $data = $response->toArray()['success'];
+            $roles = $data['roles'];
+            $email = $data['username'];
+            $user = new JWTUser($email, $roles);
+            $token = $JWTManager->create($user);
+            $returnData = ["data" => $data, "token" => $token];
+            return $this->respondWithSuccess($returnData);
+        } catch (\Exception $e) {
+            return $this->respondValidationError($e->getMessage());
+        }
+
     }
 }
