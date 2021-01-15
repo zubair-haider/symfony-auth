0ca41e1037536890a4be8c1e58574de92212a445
zubair-haider <zubair.haider@gigalabs.co>
2021-01-15 14:07:11 +0500

    updating logic

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -16,11 +16,16 @@ class UserController extends ApiController
         return $this->respondWithSuccess($token);
     }
 
-    public function userProfile(HttpClientInterface $client)
+    public function userProfile(HttpClientInterface $client, Request $request)
     {
         try {
-            $url = $_ENV['DATA_URL'] . "api/user/me";
-            $response = $client->request('GET', $url);
+            $token = $request->headers->get('Authorization');
+            $url = $_ENV['DATA_URL'] . "api/user/me/";
+            $response = $client->request('GET', $url, [
+                'headers' => [
+                    'Authorization' => $token,
+                ],
+            ]);
             $data = $response->toArray()['success'];
             return $this->respondWithSuccess($data);
         } catch (\Exception $e) {
@@ -31,11 +36,15 @@ class UserController extends ApiController
     public function userProfileUpdate(Request $request, HttpClientInterface $client)
     {
         try {
+            $token = $request->headers->get('Authorization');
             $request = $this->transformJsonBody($request);
             $roles = $request->get('roles');
             $url = $_ENV['DATA_URL'] . "api/user/me";
             $response = $client->request('PUT', $url,
                 [
+                    'headers' => [
+                        'Authorization' => $token,
+                    ],
                     'json' => ['roles' => $roles],
                 ]);
             return $this->respondWithSuccess("User updated");
@@ -47,8 +56,13 @@ class UserController extends ApiController
     public function userProfileDelete(HttpClientInterface $client)
     {
         try {
-            $url = $_ENV['DATA_URL'] . "api/user/me";
-            $response = $client->request("DELETE", $url);
+            $token = $request->headers->get('Authorization');
+            $url = $_ENV['DATA_URL'] . "api/user/me/";
+            $response = $client->request('DELETE', $url, [
+                'headers' => [
+                    'Authorization' => $token,
+                ],
+            ]);
             return $this->respondWithSuccess("user deleted");
         } catch (\Exception $e) {
             return $this->respondValidationError($e->getMessage());
